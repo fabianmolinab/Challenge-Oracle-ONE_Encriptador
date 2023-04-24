6396ade1e8b4837ec6553833f715ae5a4b125ae5
fabianmolinab <molinafabian11b@gmail.com>
2023-04-24 17:05:17 -0500

    feat: add styles mobile and responsive design :iphone:

---
+++ main.js
@@ -0,0 +1,5 @@
+/* TODO: - [] Arreglar el build de vite y la estructura de archivos
+         - [] Implementar la metodologia BEM en el css
+         - [] Estilos modo tablet
+         - [] Estilos modo mobile
+*/
